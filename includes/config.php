8573406dc0f87f1934ad7f2700c19aae6f8a2106
bram2268 <bram2268@gmail.com>
2017-04-02 23:03:38 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
   $mydomain = $_SERVER['HTTP_HOST'];
-  $domaintoproxy = "https://thepiratebay.org";
+  $domaintoproxy = "http://ahoy.one";
 
   # Add your custom code before the body (e.g. Google Analytics)
   $footerCode = <<<EOT
