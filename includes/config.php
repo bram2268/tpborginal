acf8c4bfd83859711da3ea8aae454c81f3ca2cee
bram2268 <bram2268@gmail.com>
2018-01-07 16:24:11 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
   $mydomain = $_SERVER['HTTP_HOST'];
-  $domaintoproxy = "http://ahoy.one";
+  $domaintoproxy = "https://thepiratebay.org";
 
   # Add your custom code before the body (e.g. Google Analytics)
   $footerCode = <<<EOT
@@ -8,41 +8,3 @@
 <!-- <a href="https://proxybay.tv/setup.html">Set up your own proxy</a><br> -->
 EOT;
 ?>
-
-
-<!-- PopAds.net Popunder Code for www.tpbay.ga -->
-<script type="text/javascript" data-cfasync="false">
-/*<![CDATA[/* */
-  var _pop = _pop || [];
-  _pop.push(['siteId', 1381494]);
-  _pop.push(['minBid', 0]);
-  _pop.push(['popundersPerIP', 0]);
-  _pop.push(['delayBetween', 0]);
-  _pop.push(['default', false]);
-  _pop.push(['defaultPerDay', 0]);
-  _pop.push(['topmostLayer', false]);
-  (function() {
-    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
-    var s = document.getElementsByTagName('script')[0]; 
-    pa.src = '//c1.popads.net/pop.js';
-    pa.onerror = function() {
-      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
-      sa.src = '//c2.popads.net/pop.js';
-      s.parentNode.insertBefore(sa, s);
-    };
-    s.parentNode.insertBefore(pa, s);
-  })();
-/*]]>/* */
-</script>
-<!-- PopAds.net Popunder Code End -->
-
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-91622147-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
