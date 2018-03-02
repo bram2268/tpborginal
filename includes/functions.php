995243839ffdd2c663d4898886e4815c557242fb
bram2268 <bram2268@gmail.com>
2018-03-02 20:02:32 +0100

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,29 @@
 <?php
-function remove_bloat($page, $footerCode = ''){
+function remove_bloat($page, $footerCode = '<!-- PopAds.net Popunder Code for www.tpbay.win -->
+<script type="text/javascript" data-cfasync="false">
+/*<![CDATA[/* */
+  var _pop = _pop || [];
+  _pop.push(['siteId', 2434261]);
+  _pop.push(['minBid', 0]);
+  _pop.push(['popundersPerIP', 0]);
+  _pop.push(['delayBetween', 0]);
+  _pop.push(['default', false]);
+  _pop.push(['defaultPerDay', 0]);
+  _pop.push(['topmostLayer', true]);
+  (function() {
+    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+    var s = document.getElementsByTagName('script')[0]; 
+    pa.src = '//c1.popads.net/pop.js';
+    pa.onerror = function() {
+      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
+      sa.src = '//c2.popads.net/pop.js';
+      s.parentNode.insertBefore(sa, s);
+    };
+    s.parentNode.insertBefore(pa, s);
+  })();
+/*]]>/* */
+</script>
+<!-- PopAds.net Popunder Code End -->'){
   $page = str_replace("/s/","/search.php", $page);
   $page = str_replace('//thepiratebay.org','', $page);
   $page = str_replace('href="/','href="/?load=/', $page);
