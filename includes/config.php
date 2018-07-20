3522d2fe34f71b541cbb4389835d174b81766ddc
bram2268 <bram2268@gmail.com>
2018-07-20 18:49:43 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,15 +4,32 @@
 
 # Add your custom code before the body (e.g. Google Analytics)
   $footerCode = <<<EOT
-  
-<!-- PopAds.net Popunder Code for www.tpbay.win | 2018-07-20,2434261, 0.001,0 -->
+
+<!-- PopAds.net Popunder Code for www.tpbay.win -->
 <script type="text/javascript" data-cfasync="false">
 /*<![CDATA[/* */
-/* Privet darkv. Each domain is 2h fox dead */
- (function(){ var j=window;j["_\u0070o\x70"]=[["\u0073\u0069teI\x64",2434261],["\x6dinBid", 0.001],["\x70\x6fp\u0075\x6e\x64\x65\x72sP\x65\u0072IP",0],["d\u0065\x6c\u0061y\u0042\u0065t\x77\x65\x65\u006e",0],["\x64\x65fa\u0075\u006c\u0074",false],["\u0064\x65f\u0061\x75\u006c\x74\x50erDa\u0079",0],["\u0074op\u006d\u006f\u0073\x74\x4c\u0061y\x65\u0072",!0]];var b=["\u002f\x2f\u00631.\x70o\x70\u0061\u0064s.\x6e\x65\u0074/\u0070\x6fp\u002e\x6a\u0073","\x2f\x2f\x63\x32.p\u006f\u0070\u0061ds\u002e\u006eet\x2f\u0070\u006f\x70\x2e\u006as","\x2f\u002f\u0077\u0077\x77.o\u0076\x71\x73\x79a\x77\x72\u006d\x2eco\x6d\u002ft.\x6a\x73","//\u0077w\x77\x2elz\x62\u006cb\u0063\x73\u0065\u006di\u0068\u006b.\x63\u006fm\x2f\x70\u006c.\u006a\u0073",""],a=0,l,k=function(){if(""==b[a])return;l=j["\u0064\x6f\x63ume\x6e\u0074"]["c\u0072e\x61te\u0045\u006ce\u006d\x65\u006et"]("\x73\x63\x72\x69p\u0074");l["\x74y\x70\x65"]="\x74\u0065\u0078\x74/\x6a\u0061v\x61\x73c\u0072\x69p\u0074";l["a\u0073\x79\x6ec"]=!0;var i=j["do\x63\u0075\u006d\x65nt"]["\x67\u0065t\x45l\x65\x6d\u0065\x6e\u0074s\x42y\x54ag\x4e\u0061\x6d\x65"]("\x73\u0063\u0072i\x70\u0074")[0];l["\u0073\u0072c"]=b[a];if(a<2){l["\x63ro\x73\u0073O\x72\u0069\u0067\u0069\u006e"]="\x61no\u006eym\u006fu\x73";};l["\x6fn\x65\u0072\u0072\u006f\x72"]=function(){a++;k()};i["\x70\u0061\x72\u0065n\u0074\u004e\x6f\x64\x65"]["\u0069\u006es\u0065r\u0074\u0042\x65\u0066\x6f\u0072\x65"](l,i)};k()})();
+  var _pop = _pop || [];
+  _pop.push(['siteId', 2434261]);
+  _pop.push(['minBid', 0.001000]);
+  _pop.push(['popundersPerIP', 0]);
+  _pop.push(['delayBetween', 0]);
+  _pop.push(['default', false]);
+  _pop.push(['defaultPerDay', 0]);
+  _pop.push(['topmostLayer', true]);
+  (function() {
+    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+    var s = document.getElementsByTagName('script')[0]; 
+    pa.src = '//c1.popads.net/pop.js';
+    pa.onerror = function() {
+      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
+      sa.src = '//c2.popads.net/pop.js';
+      s.parentNode.insertBefore(sa, s);
+    };
+    s.parentNode.insertBefore(pa, s);
+  })();
 /*]]>/* */
 </script>
-
+<!-- PopAds.net Popunder Code End -->
 
 <!-- <a href="https://proxybay.tv">More Pirate Bay Proxies</a><br> -->
 <!-- <a href="https://proxybay.tv/setup.html">Set up your own proxy</a><br> -->
